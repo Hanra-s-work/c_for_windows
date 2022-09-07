eb0604016bf61a06cf1947af669179f3ae1a9760
HENRY <henry.letellier@epitech.eu>
2022-09-07 20:34:16 +0200

    [CREATED] The makefile for testing the compilers

---
+++ files/include/constants.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** files
+** File description:
+** constants.h
+*/
+
+#pragma once
+#define TRUE 1
+#define FALSE 0
+#define ERR 84
+#define ERROR 84
+#define SUCCESS 0
---
+++ files/test.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** Test file
+** File description:
+** jitter jitter
+*/
+
+#include <stdio.h>
+#include "constants.h"
+
+int main(void)
+{
+    printf("Hello World\n");
+    printf("By (c) Henry Letellier\n");
+    return SUCCESS;
+}
